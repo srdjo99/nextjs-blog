f24996aae94a707a064b38c1a7904ed07f435555
Srdjan <srdjancoralic5@gmail.com>
2022-10-25 12:42:54 +0200

    App test

---
+++ components/date.tsx
@@ -0,0 +1,6 @@
+import { format, parseISO } from "date-fns";
+
+export default function Date({ dateString }: any) {
+  const date = parseISO(dateString);
+  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
+}
